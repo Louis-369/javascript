4b158bb6e5239ac6bee7c86f41dbfc10cedc8a2f
Louis <9313louis@gmail.com>
2025-10-17 16:26:59 +0800

    Add BMI calculation function and update comments in ff.js

--- 1017-JS/ff.js
+++ 1017-JS/ff.js
@@ -11,3 +11,17 @@ sayHelloTo(1, 2, 3, 4); // 呼叫執行 sayHelloTo(引數argument)
 // 缺：undenfined
 // ... rest parameter
 // 參數加＝可設預設值 如果沒引數可用
+// 沒加return印出來會是undefined
+// f(x)=3x+2
+// 3 => 11回傳值
+// 回傳值一定要印出來
+
+// 計算BMI
+// 體重/身高＾2，身高用公尺單位
+function calcBMI(height, weight) {
+  const h = height / 100;
+  const result = weight / (h * h);
+  return result;
+}
+const bmi = calcBMI(180, 80);
+console.log(bmi);
