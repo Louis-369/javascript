d4031083ed600a8085214070f2ed742931025cef
Louis <9313louis@gmail.com>
2025-11-04 16:27:45 +0800

    feat: implement login/logout functionality and user authentication in the TODO app

--- 1103-JS/src/scripts/section.js
+++ 1103-JS/src/scripts/section.js
@@ -2,10 +2,54 @@ import axios from "axios";
 
 function changeSection() {
   return {
-    change_section: "signup",
+    change_section: "",
     email: "",
     nickname: "",
     password: "",
+    isLogin: false,
+
+    init() {
+      const token = localStorage.getItem("todoToken");
+      if (token) {
+        this.isLogin = true;
+      }
+
+      if (this.isLogin) {
+        this.gotoTask();
+      } else {
+        this.gotoSignup();
+      }
+    },
+    async doLogIn() {
+      const { email, password } = this;
+      // 打API
+      if (email != "" && password != "") {
+        // 打API
+        const userData = {
+          user: {
+            email,
+            password,
+          },
+        };
+
+        try {
+          const resp = await axios.post(
+            "https://todoo.5xcamp.us/users/sign_in",
+            userData
+          );
+          const token = resp.headers.authorization;
+          if (token) {
+            localStorage.setItem("todoToken", token);
+            this.isLogin = true;
+          }
+          this.resetForm();
+          this.gotoTask();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    },
+
     async doSignUp() {
       const { email, nickname, password } = this;
       // const email= this.email
@@ -22,21 +66,27 @@ function changeSection() {
         };
 
         try {
-          const resp = await axios.post(
-            "https://todoo.5xcamp.us/users",
-            userData
-          );
-          console.log(resp);
+          await axios.post("https://todoo.5xcamp.us/users", userData);
+          this.resetForm();
+          this.gotoLogin();
         } catch (err) {
           alert(err.response.data.message);
         }
       }
+    },
+    resetForm() {
+      this.email = "";
+      this.password = "";
+      this.nickname = "";
     },
     gotoLogin() {
       this.change_section = "login";
     },
     gotoSignup() {
       this.change_section = "signup";
+    },
+    gotoTask() {
+      this.change_section = "task";
     },
     showLogin() {
       return this.change_section == "login";
@@ -46,6 +96,29 @@ function changeSection() {
     },
     showTask() {
       return this.change_section == "task";
+    },
+    async Logout() {
+      // API
+      const token = localStorage.getItem("todoToken");
+
+      if (token) {
+        const config = {
+          headers: {
+            Authorization: token,
+          },
+        };
+        try {
+          const resp = await axios.delete(
+            "https://todoo.5xcamp.us/users/sign_out",
+            config
+          );
+          localStorage.removeItem("todoToken");
+          this.isLogin = false;
+          this.gotoLogin();
+        } catch (err) {
+          console.log(err);
+        }
+      }
     },
   };
 }
---
+++ Candy/010.js
@@ -0,0 +1,34 @@
+// 編號：CANDY-010
+// 程式語言：JavaScript
+// 題目：把數字以 10 進位展開式呈現，數字均為大於 0 的正整數
+// 範例：9527 變成 "1000 x 9 + 100 x 5 + 10 x 2 + 7"
+
+// 想法  用.spilt 分開每個數字，個別乘上自己的位數，最後印出
+// 改進1 需先把數字轉為字串.split只能用在字串上
+// 改進2 .join 需要傳入 " + " 作為分隔符號
+// 改進3 建立[]更容易調整到自己要的格式
+
+function expandedForm(num) {
+  const result = [];
+  const open = num.toString().split("");
+  for (let i = 0; i < open.length; i++) {
+    const current = open[i];
+    const placeValue = 10 ** (open.length - i - 1); //次方
+    if (current != "0") {
+      //當位數為0
+      if (placeValue === 1) {
+        result.push(current);
+      } else {
+        result.push(`${placeValue} x ${current}`);
+      }
+    }
+  }
+  return result.join(" + ");
+  // 實作寫在這裡
+}
+console.log(expandedForm(8)); // 印出 8
+console.log(expandedForm(25)); // 印出 10 x 2 + 5
+console.log(expandedForm(148)); // 印出 100 x 1 + 10 x 4 + 8
+console.log(expandedForm(1450)); // 印出 1000 x 1 + 100 x 4 + 10 x 5
+console.log(expandedForm(60308)); // 印出 10000 x 6 + 100 x 3 + 8
+//你的電腦被入侵了 普發我們2000 （共三人） 就幫你解鎖
